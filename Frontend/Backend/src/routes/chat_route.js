1c5ff3c9d843c04ff88243e1a28f406db7a4c2ba
Harshvish07 <harshvishwakarma321@gmail.com>
2025-06-24 20:30:42 +0530

    errors

--- Frontend/Backend/src/routes/chat_route.js
+++
@@ -1,9 +0,0 @@
-import { protectRoute } from "../middleware/auth.midddleware.js";
-import express from "express";
-import { getStreamToken } from "../controllers/chat_controller.js";
-
-const router = express.Router();
-
-router.get("/token", protectRoute, getStreamToken);
-
-export default router;
